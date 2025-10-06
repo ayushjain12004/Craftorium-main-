bd763a71b34945c7db667423a1b420f57fac2110
ayush jain <ayushjainkaju@gmail.com>
2025-10-06 22:05:53 +0530

    UI create

---
+++ src/app/cart-component/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <>
+     
+     
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+// components/Loading.jsx or Loading.tsx
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center">
+      {/* Your loading spinner or skeleton */}
+      <div>Loading...</div>
+    </div>
+  );
+};
+
+// Make sure it's exported as default
+export default Loading;
